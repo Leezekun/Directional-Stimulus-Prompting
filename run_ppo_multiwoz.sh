93b44f9e74d608732fd7809e664cdc6c9f1f769b
leezekun <lizekunlee@gmail.com>
2023-05-17 00:47:08 -0400

    update

--- run_ppo_multiwoz.sh
+++ run_ppo_multiwoz.sh
@@ -4,5 +4,5 @@ gpu=0,1,2
 CUDA_VISIBLE_DEVICES=$gpu python scripts/training/train_text_generation.py \
     --base_path_to_store_results ./rl4lms_exps \
     --project_name multiwoz_with_hint \
-    --experiment_name flan-t5-large_nlpo_on_supervised_2.0_400 \
-    --config_path scripts/training/task_configs/multiwoz_with_hint/flan-t5_nlpo_on_supervised_2.0_400.yml
+    --experiment_name flan-t5-large_nlpo_on_supervised_2.0_80 \
+    --config_path scripts/training/task_configs/multiwoz_with_hint/flan-t5_nlpo_on_supervised_2.0_80.yml
